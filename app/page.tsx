8228ebcef5bdc28cdad1830fc94569f273021c9f
Vintersorganic <jorge.gdp@gmail.com>
2023-11-03 14:05:49 -0300

    Explore button on home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,20 +6,30 @@ import Spline from '@splinetool/react-spline';
 import './page.css';
 
 const HomePage = () => {
-  const [isMounted, setIsMounted] = useState(false);
+  const [isSplineLoaded, setIsSplineLoaded] = useState(false);
 
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
+  const handleSplineLoad = () => {
+    setIsSplineLoaded(true);
+  };
 
   return (
     <BasePage navbar={false} footer={true} className="relative flex flex-col min-h-screen">
-      <div className="absolute inset-0 z-0">
-        <Spline scene="https://prod.spline.design/8MZRGCAOH9d0Kg0p/scene.splinecode" />
+      {!isSplineLoaded && (
+        <div className="fixed inset-0 z-50 flex justify-center items-center bg-white">
+          Cargando...
+        </div>
+      )}
+      <div className={`absolute inset-0 z-0 ${isSplineLoaded ? '' : 'hidden'}`}>
+        <Spline 
+          scene="https://prod.spline.design/8MZRGCAOH9d0Kg0p/scene.splinecode" 
+          onLoad={handleSplineLoad} 
+        />
       </div>
-      <main className="flex-grow">
-        <Home />
-      </main>
+      {isSplineLoaded && (
+        <main className="flex-grow">
+          <Home />
+        </main>
+      )}
     </BasePage>
   );
 }
--- src/screens/home/AnimatedText.tsx
+++ src/screens/home/AnimatedText.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useSpring, animated } from 'react-spring';
 import TitleSection from '../components/texts/TitleSection';
 import { TextColors } from 'src/utils/Colors';
@@ -6,15 +6,31 @@ import './styles.css';
 import TextIcon, { SizeIcons, TextIcons } from '../components/icons/TextIcon';
 import Link from 'next/link';
 import { useAppSelector } from 'src/redux/hooks';
+import ReactDOM from 'react-dom';
+import TitleSmaller from '../components/texts/TitleSmaller';
 
 const carouselTexts = ['WORLDWIDE', 'FOR EUROPE', 'FOR SPAIN', 'LATIN AMERICA', 'SPAIN & LATAM'];
 
 const AnimatedText = () => {
     const [index, setIndex] = useState(0);
     const artists = useAppSelector(state => state.artistsReducer.data);
+    const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+    const containerRef = useRef(null);
+
+    useEffect(() => {
+        const moveCursor = (e) => {
+            setCursorPosition({ x: e.clientX, y: e.clientY });
+        };
+        window.addEventListener('mousemove', moveCursor);
+        return () => {
+            window.removeEventListener('mousemove', moveCursor);
+        };
+    }, []);
 
     const groupedArtists = [];
-    if (!artists) return <></>   
+
+    if (!artists) return <></>;
+
     for (let i = 0; i < artists.length; i += 4) {
         groupedArtists.push(artists.slice(i, i + 4));
     }
@@ -32,53 +48,65 @@ const AnimatedText = () => {
         return () => clearInterval(interval);
     }, []);
 
-    return (
-        <div className="relative h-screen bg-black overflow-hidden">
-                   <div className="animated-text z-10">
-            {groupedArtists.map((group, index) => (
-                <TitleSection 
-                    key={index} 
-                    text={group.map(artist => artist.name.toUpperCase()).join('. ')} 
-                    color={TextColors.gray} 
-                    className='mb-2 whitespace-nowrap overflow-hidden ml-[-25%]' 
-                />
-            ))}
-        </div>
-        <div className="animated-text animated-text-delayed-1 z-10">
-            {groupedArtists.map((group, index) => (
-                <TitleSection 
-                    key={index} 
-                    text={group.map(artist => artist.name.toUpperCase()).join('. ')} 
-                    color={TextColors.gray} 
-                    className='mb-2 whitespace-nowrap overflow-hidden ml-[-25%]' 
-                />
-            ))}
-        </div>
-        <div className="animated-text animated-text-delayed-2 z-10">
-            {groupedArtists.map((group, index) => (
-                <TitleSection 
-                    key={index} 
-                    text={group.map(artist => artist.name.toUpperCase()).join('. ')} 
-                    color={TextColors.gray} 
-                    className='mb-2 whitespace-nowrap overflow-hidden ml-[-25%]' 
-                />
-            ))}
-        </div>
-        <div className="animated-text animated-text-delayed-3 z-10">
-            {groupedArtists.map((group, index) => (
-                <TitleSection 
-                    key={index} 
-                    text={group.map(artist => artist.name.toUpperCase()).join('. ')} 
-                    color={TextColors.gray} 
-                    className='mb-2 whitespace-nowrap overflow-hidden ml-[-25%]' 
-                />
-            ))}
-        </div>
+    const cursorComponent = (<div
+        className="fixed w-32 h-32 bg-white bg-opacity-10 rounded-full pointer-events-none z-50 pointer"
+        style={{
+            top: `${cursorPosition.y - 78}px`,
+            left: `${cursorPosition.x - 64}px`,
+            zIndex: 9999
+        }}
+    >
+        <TitleSmaller text={'EXPLORE'} className="inter opacity-100 desk:text-[14px] absolute top-[50%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white" />
+    </div>)
 
+    return (
+        <div className="relative h-screen bg-black overflow-hidden pointer" >
+            {ReactDOM.createPortal(cursorComponent, document.body)}
+
+            <div className="animated-text z-10">
+                {groupedArtists.map((group, index) => (
+                    <TitleSection
+                        key={index}
+                        text={group.map(artist => artist.name.toUpperCase()).join('. ')}
+                        color={TextColors.gray}
+                        className='mb-2 whitespace-nowrap overflow-hidden ml-[-25%]'
+                    />
+                ))}
+            </div>
+            <div className="animated-text animated-text-delayed-1 z-10">
+                {groupedArtists.map((group, index) => (
+                    <TitleSection
+                        key={index}
+                        text={group.map(artist => artist.name.toUpperCase()).join('. ')}
+                        color={TextColors.gray}
+                        className='mb-2 whitespace-nowrap overflow-hidden ml-[-25%]'
+                    />
+                ))}
+            </div>
+            <div className="animated-text animated-text-delayed-2 z-10">
+                {groupedArtists.map((group, index) => (
+                    <TitleSection
+                        key={index}
+                        text={group.map(artist => artist.name.toUpperCase()).join('. ')}
+                        color={TextColors.gray}
+                        className='mb-2 whitespace-nowrap overflow-hidden ml-[-25%]'
+                    />
+                ))}
+            </div>
+            <div className="animated-text animated-text-delayed-3 z-10">
+                {groupedArtists.map((group, index) => (
+                    <TitleSection
+                        key={index}
+                        text={group.map(artist => artist.name.toUpperCase()).join('. ')}
+                        color={TextColors.gray}
+                        className='mb-2 whitespace-nowrap overflow-hidden ml-[-25%]'
+                    />
+                ))}
+            </div>
             <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black z-20" />
             <div className="absolute bottom-0 z-30 text-white pl-4">
-                <Link href="/artists" className='flex flex-row'>
-                    <TitleSection text={`ARTISTS`} color={TextColors.white} />
+                <Link href="/artists" className='flex flex-row customLink'>
+                    <TitleSection text={`ARTISTS`} color={TextColors.white}  className='textBesideIcon'/>
                     <TextIcon icon={TextIcons.RIGHT_ARROW} size={SizeIcons.TITLE} color={TextColors.white} />
                 </Link>
                 <div className="overflow-hidden" style={{ width: '100%' }}>
