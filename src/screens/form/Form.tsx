abe5ff8b9e8c49a6193c015d28e6ed4f43c724e5
pizzars <jyjdajj@gmail.com>
2023-11-07 15:27:55 -0500

    Ajustes de deploy

--- src/screens/form/Form.tsx
+++ src/screens/form/Form.tsx
@@ -7,7 +7,7 @@ import Promoter from './steps/Promoter'
 import Venue from './steps/Venue'
 
 const Form = () => {
-  const [step, setStep] = useState(1)
+  const [step, setStep] = useState(0)
   const [form, setForm] = useState<Partial<FormRequest>>({})
 
   const updateData = (data: Partial<FormRequest>, newStep: number) => {
