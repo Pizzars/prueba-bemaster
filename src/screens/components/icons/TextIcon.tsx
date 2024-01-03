31ae79fae2a3b5352d9ee2f079b13ce92cc5fdfb
pizzars <jyjdajj@gmail.com>
2024-01-03 00:51:33 -0500

    Animación de flechas en los links

--- src/screens/components/icons/TextIcon.tsx
+++ src/screens/components/icons/TextIcon.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { TextColors } from 'src/utils/Colors'
 import TextBase, { Fonts, TextTags } from '../texts/TextBase'
 import TitleHome from '../texts/TitleHome'
@@ -8,6 +9,7 @@ import TextParagraph from '../texts/TextParagraph'
 import TextSmall from '../texts/TextSmall'
 import CalendarIcon from './CalendarIcon'
 import CheckBoxIcon from './CheckBoxIcon'
+import { useRef, useState } from 'react'
 
 export enum TextIcons {
   DIAGONAL_ARROW = '↗',
@@ -52,71 +54,123 @@ const TextIcon = ({
     return <CheckBoxIcon checked={checked} className={className} />
   }
 
-  switch (size) {
-    case SizeIcons.TITLE_HOME: {
-      return <TitleHome tag={TextTags.SPAN} text={icon} color={color} font={Fonts.inter} />
-    }
-    case SizeIcons.TITLE: {
-      return (
-        <TitleSection
-          tag={TextTags.SPAN}
-          text={icon}
-          color={color}
-          font={Fonts.inter}
-          className={`${className} font-bold`}
-        />
-      )
-    }
-    case SizeIcons.TITLE_MEDIUM: {
-      return (
-        <TitleMedium
-          tag={TextTags.SPAN}
-          text={icon}
-          color={color}
-          font={Fonts.inter}
-          className={`${className} font-bold`}
-        />
-      )
-    }
-    case SizeIcons.TITLE_SMALL: {
-      return (
-        <TitleSmall
-          tag={TextTags.SPAN}
-          text={icon}
-          color={color}
-          font={Fonts.inter}
-          className={`${className} font-bold`}
-        />
-      )
-    }
-    case SizeIcons.TEXT_PARAGRAPH: {
-      return (
-        <TextParagraph
-          tag={TextTags.SPAN}
-          text={icon}
-          color={color}
-          font={Fonts.inter}
-          className={`${className} font-bold`}
-        />
-      )
-    }
-    case SizeIcons.TEXT_SMALL: {
-      return (
-        <TextSmall
-          tag={TextTags.SPAN}
-          text={icon}
-          color={color}
-          font={Fonts.inter}
-          className={`${className} font-bold`}
-        />
-      )
-    }
-    default: {
-      return (
-        <TextBase text={icon} className={`text-[14px] inter font-bold ${color} ${className}`} />
-      )
+  const [isHovered, setIsHovered] = useState(false)
+  const [translate, setTranslate] = useState({ x: 0, y: 0 })
+  const ref = useRef<null | HTMLSpanElement>(null)
+
+  const handleMouseEnter = (e: any) => {
+    setIsHovered(true)
+    updatePosition(e)
+  }
+
+  const handleMouseLeave = () => {
+    setIsHovered(false)
+    setTranslate({ x: 0, y: 0 })
+  }
+
+  const handleMouseMove = (e: any) => {
+    if (isHovered) {
+      updatePosition(e)
     }
   }
+
+  const updatePosition = (e: any) => {
+    const container = ref.current
+    if (!container) return
+    const { top, left } = container.getBoundingClientRect()
+    const width = container.offsetWidth / 2
+    const height = container.offsetHeight / 2
+    setTranslate({ x: e.clientX - left - width, y: e.clientY - top - height })
+  }
+
+  const getView = () => {
+    switch (size) {
+      case SizeIcons.TITLE_HOME: {
+        return <TitleHome tag={TextTags.SPAN} text={icon} color={color} font={Fonts.inter} />
+      }
+      case SizeIcons.TITLE: {
+        return (
+          <TitleSection
+            tag={TextTags.SPAN}
+            text={icon}
+            color={color}
+            font={Fonts.inter}
+            className={`${className} font-bold`}
+          />
+        )
+      }
+      case SizeIcons.TITLE_MEDIUM: {
+        return (
+          <TitleMedium
+            tag={TextTags.SPAN}
+            text={icon}
+            color={color}
+            font={Fonts.inter}
+            className={`${className} font-bold`}
+          />
+        )
+      }
+      case SizeIcons.TITLE_SMALL: {
+        return (
+          <TitleSmall
+            tag={TextTags.SPAN}
+            text={icon}
+            color={color}
+            font={Fonts.inter}
+            className={`${className} font-bold`}
+          />
+        )
+      }
+      case SizeIcons.TEXT_PARAGRAPH: {
+        return (
+          <TextParagraph
+            tag={TextTags.SPAN}
+            text={icon}
+            color={color}
+            font={Fonts.inter}
+            className={`${className} font-bold`}
+          />
+        )
+      }
+      case SizeIcons.TEXT_SMALL: {
+        return (
+          <TextSmall
+            tag={TextTags.SPAN}
+            text={icon}
+            color={color}
+            font={Fonts.inter}
+            className={`${className} font-bold`}
+          />
+        )
+      }
+      default: {
+        return (
+          <TextBase text={icon} className={`text-[14px] inter font-bold ${color} ${className}`} />
+        )
+      }
+    }
+  }
+
+  return (
+    <span
+      className='inline-block'
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onMouseMove={handleMouseMove}
+      ref={ref}
+    >
+      <span
+        className='inline-block'
+        style={{
+          pointerEvents: 'none',
+          transform: `translate(${translate.x}px, ${translate.y}px)`,
+          transition: '0.3s all ease'
+        }}
+      >
+        {getView()}
+      </span>
+    </span>
+  )
 }
 
 export default TextIcon
